b9d8ad4bc5a76202047408ecc916addf0f3aa296
Adolfo Huerta <102195837+AdolfoHMtz@users.noreply.github.com>
2025-11-09 00:20:51 -0600

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   url: 'https://api.themoviedb.org/3',
-  apiKey: 'd25b43b53d2d091d30eed7d1815e566f',
+  apiKey: '',
   imgPath: 'https://image.tmdb.org/t/p'
 };
